03790068acf6dced7d54284c878e9ba7bde1641e
Sung-Hwi <75319719+halo-wi@users.noreply.github.com>
2021-03-25 21:04:14 +0900

    Add files via upload

---
+++ MeetingRoomsVO.java
@@ -0,0 +1,34 @@
+package MeetingReservationModel;
+
+public class MeetingRoomsVO {
+	int meeting_room_id;
+	int meeting_capacity;
+	public int getMeeting_room_id() {
+		return meeting_room_id;
+	}
+	public void setMeeting_room_id(int meeting_room_id) {
+		this.meeting_room_id = meeting_room_id;
+	}
+	public int getMeeting_capacity() {
+		return meeting_capacity;
+	}
+	public void setMeeting_capacity(int meeting_caracity) {
+		this.meeting_capacity = meeting_caracity;
+	}
+	public MeetingRoomsVO(int meeting_room_id, int meeting_caracity) {
+		super();
+		this.meeting_room_id = meeting_room_id;
+		this.meeting_capacity = meeting_caracity;
+	}
+	public MeetingRoomsVO() {
+		super();
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("회의실 정보 [meeting_room_id=").append(meeting_room_id).append(", meeting_caracity=")
+				.append(meeting_capacity).append("]");
+		return builder.toString();
+	}
+
+}
